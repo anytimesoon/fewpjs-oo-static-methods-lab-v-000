67cb59147b6ea056a24313ba155ade02a23274e0
Damien de Lartigue <damien@delartigue.co.uk>
2021-03-12 19:01:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 class Formatter {
   //add static methods here
   static capitalize(str) {
-    return str.charAt(0).toUpperCase() + str.slice(1);
+    str = str.charAt(0).toUpperCase() + str.slice(1)
+    console.log('str');
+    return str;
+    
   }
   
   static sanitize(str) {
