9fddb22ab733d8d77d199de51703a1cc6d3c048a
Damien de Lartigue <damien@delartigue.co.uk>
2021-03-12 18:54:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,34 +15,35 @@ class Formatter {
       if (i === 0) {
         self.capitalize(words[i]);
       }
+    }
       
-    //   switch(words[i]){
-    //     case 'a':
-    //       break;
-    //     case 'an':
-    //       break;
-    //     case 'but':
-    //       break;
-    //     case 'of':
-    //       break;
-    //     case 'and':
-    //       break;
-    //     case 'for':
-    //       break;
-    //     case 'at':
-    //       break;
-    //     case 'by':
-    //       break;
-    //     case 'from':
-    //       break;
-    //     default:
-    //       self.capitalize(words[i]);
-    //       break;
-    //   }
-    // }
+      switch(words[i]){
+        case 'a':
+          break;
+        case 'an':
+          break;
+        case 'but':
+          break;
+        case 'of':
+          break;
+        case 'and':
+          break;
+        case 'for':
+          break;
+        case 'at':
+          break;
+        case 'by':
+          break;
+        case 'from':
+          break;
+        default:
+          self.capitalize(words[i]);
+          break;
+      }
+    
     
     return words.join(' ');
-  }
+  
 }
 
 for (i = 0; i < cars.length; i++) {
